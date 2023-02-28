2c323e22158f8975e251e9285bdd137653554903
Joe Frey <jjfrey@gmail.com>
2023-02-27 19:50:38 -0500

    second

---
+++ primary_table.rb
@@ -0,0 +1,32 @@
+class PrimaryTable 
+
+    def is_prime(number)
+      return true if [0,1,2,3].include?(number)
+      return false if number % 2 == 0 
+
+      return true
+    end
+
+    def sieve(number)
+      primes_numbers = (0..number).to_a
+      primes_numbers[0] = primes_numbers[1] = nil
+    
+      primes_numbers.each do |prime_candidate|
+        next unless prime_candidate
+
+        candidate_squared = prime_candidate * prime_candidate
+        break if candidate_squared > number
+        
+        (candidate_squared).step(number,prime_candidate).each do |x|
+          primes_numbers[x] = nil
+        end
+      end
+
+      return primes_numbers.compact
+    end
+
+    def initialize(number)
+        
+    end
+    
+end
