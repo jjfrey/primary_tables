66eda29d9280c83eedfea6a9ddfe309614aa6619
Joe Frey <jjfrey@gmail.com>
2023-02-28 18:41:05 -0500

    wip

---
+++ test_primary_table.rb
@@ -0,0 +1,23 @@
+require_relative "primary_table"
+require "test/unit"
+ 
+class TestPrimaryTable < Test::Unit::TestCase
+ 
+  def test_is_prime
+    pt = PrimaryTable.new(1)
+    assert_equal(true, pt.is_prime(1))
+    assert_equal(true, pt.is_prime(2))
+    assert_equal(true, pt.is_prime(3))
+    assert_equal(false, pt.is_prime(4))
+    assert_equal(false, pt.is_prime(6))
+    assert_equal(true, pt.is_prime(11))
+    assert_equal(true, pt.is_prime(13))
+    assert_equal(false, pt.is_prime(15))
+    assert_equal(true, pt.is_prime(17))
+    assert_equal(false, pt.is_prime(21))
+    assert_equal(true, pt.is_prime(23))
+    assert_equal(false, pt.is_prime(27))
+    assert_equal(false, pt.is_prime(36))
+  end
+ 
+end
