5a40d17029c7eb58a669520f80b9c590aea73021
Joe Frey <jjfrey@gmail.com>
2023-02-28 19:43:37 -0500

    changing prime definition

--- test_primary_table.rb
+++ test_primary_table.rb
@@ -22,8 +22,9 @@ class TestPrimaryTable < Test::Unit::TestCase
 
   def test_get_primes_for
     pt = PrimaryTable.new(1)
-    assert_equal([2,3], pt.get_primes_for(2))
-    assert_equal([2,3,5], pt.get_primes_for(3))
+    assert_equal([2,3,5], pt.get_primes_for(2))
+    assert_equal([2,3,5,7], pt.get_primes_for(3))
+    assert_equal([2,3,5,7,11], pt.get_primes_for(4))
   end
  
 end
